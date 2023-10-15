9bce7b023915bc7c2e596a9cd748118e3072b9f1
Kasualix <670067575@qq.com>
2023-10-15 15:55:44 +0800

    Initial commit

---
+++ src/main/java/com/teampotato/fastfight/FastFight.java
@@ -0,0 +1,8 @@
+package com.teampotato.fastfight;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(FastFight.MOD_ID)
+public class FastFight {
+    public static final String MOD_ID = "fastfight";
+}
---
+++ src/main/java/com/teampotato/fastfight/mixin/LocalPlayerPatchMixin.java
@@ -0,0 +1,18 @@
+package com.teampotato.fastfight.mixin;
+
+import net.minecraft.client.player.LocalPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import yesman.epicfight.client.world.capabilites.entitypatch.player.AbstractClientPlayerPatch;
+import yesman.epicfight.client.world.capabilites.entitypatch.player.LocalPlayerPatch;
+
+import java.util.List;
+
+@Mixin(LocalPlayerPatch.class)
+public abstract class LocalPlayerPatchMixin extends AbstractClientPlayerPatch<LocalPlayer> {
+    @Redirect(method = "updateHeldItem", remap = false, at = @At(value = "INVOKE", remap = false, target = "Ljava/util/List;contains(Ljava/lang/Object;)Z", ordinal = 1))
+    private boolean onCheckMiningItem(List<?> instance, Object o) {
+        return this.playerMode != PlayerMode.MINING;
+    }
+}
