4861b034f015a7eb99154a98d0e564d4b006a6c2
Kasualix <670067575@qq.com>
2023-10-16 14:26:38 +0800

    all in one

--- src/main/java/com/teampotato/autochangefight/AutoChangeFight.java
+++ src/main/java/com/teampotato/autochangefight/AutoChangeFight.java
@@ -1,15 +1,8 @@
 package com.teampotato.autochangefight;
 
-import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.NetworkConstants;
 
 @Mod(AutoChangeFight.MOD_ID)
 public class AutoChangeFight {
     public static final String MOD_ID = "autochangefight";
-
-    public AutoChangeFight() {
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
-    }
 }
--- src/main/java/com/teampotato/autochangefight/mixin/LocalPlayerPatchMixin.java
+++ src/main/java/com/teampotato/autochangefight/mixin/LocalPlayerPatchMixin.java
@@ -1,6 +1,5 @@
 package com.teampotato.autochangefight.mixin;
 
-import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -10,7 +9,8 @@ import yesman.epicfight.client.world.capabilites.entitypatch.player.LocalPlayerP
 import java.util.List;
 
 @Mixin(LocalPlayerPatch.class)
-public abstract class LocalPlayerPatchMixin extends AbstractClientPlayerPatch<LocalPlayer> {
+@SuppressWarnings("rawtypes")
+public abstract class LocalPlayerPatchMixin extends AbstractClientPlayerPatch {
     @Redirect(method = "updateHeldItem", remap = false, at = @At(value = "INVOKE", remap = false, target = "Ljava/util/List;contains(Ljava/lang/Object;)Z", ordinal = 1))
     private boolean onCheckMiningItem(List<?> instance, Object o) {
         return this.playerMode != PlayerMode.MINING;
